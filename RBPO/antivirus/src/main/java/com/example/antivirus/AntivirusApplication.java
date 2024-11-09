762873d7b700bc6b2ead7469aa0f0a1a8b3723f7
Feijoa09 <154075245+Feijoa09@users.noreply.github.com>
2024-11-09 22:16:41 +0300

    Add files via upload

---
+++ RBPO/antivirus/src/main/java/com/example/antivirus/AntivirusApplication.java
@@ -0,0 +1,12 @@
+package com.example.antivirus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntivirusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntivirusApplication.class, args);
+    }
+}
